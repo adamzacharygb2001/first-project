f5f990213ced15333e1a709f8d76da947bceb12d
adambulatao2001 <adam.bulatao@student.ateneo.edu>
2024-07-28 16:33:47 +0800

    commit

---
+++ src/components/accordion/data.js
@@ -0,0 +1,19 @@
+const data = [
+
+    {
+        id : '1',
+        question: "what are accordion components?",
+        answer:
+            "Dummy answer"
+    },
+
+    {
+    id : '2',
+    question: "What are they used for?",
+    answer:
+        "Dummy answer 2"
+    },
+
+];
+
+export default data;
